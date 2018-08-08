af68eb3fcd2ebf3bbaf8dca29e5f91b1bebc371a
ShreyaSingh10 <shreyasssingh10gmail.com>
2018-08-08 22:44:31 +0530

    refactoring

--- src/Components/DisplayWeather.js
+++ src/Components/DisplayWeather.js
@@ -8,14 +8,33 @@ export default class DisplayWeather extends React.Component {
     return (
       <div>
         {data && (
-         <div className="displayWeather"> 
-         	<h2><font face="Comic sans MS" size="5">{this.props.city} </font></h2>     
-            <p> <b>Date and Time:</b> {data.dt_txt}</p>
-            <p> <b>Temperature Maximum:</b> {conversion(data.main.temp_max)} °C</p>
-            <p> <b>Temperature Minimum:</b> {conversion(data.main.temp_min)} °C</p>
-            <p> <b>Humidity:</b> {data.main.humidity}</p>
-            <p> <b>Rain:</b> {data.weather[0].description}</p>
-  		</div>
+          <div className="displayWeather">
+            <h2>
+              <font face="Comic sans MS" size="5">
+                {this.props.city}{' '}
+              </font>
+            </h2>
+            <p>
+              {' '}
+              <b>Date and Time:</b> {data.dt_txt}
+            </p>
+            <p>
+              {' '}
+              <b>Temperature Maximum:</b> {conversion(data.main.temp_max)} °C
+            </p>
+            <p>
+              {' '}
+              <b>Temperature Minimum:</b> {conversion(data.main.temp_min)} °C
+            </p>
+            <p>
+              {' '}
+              <b>Humidity:</b> {data.main.humidity}
+            </p>
+            <p>
+              {' '}
+              <b>Rain:</b> {data.weather[0].description}
+            </p>
+          </div>
         )}
       </div>
     );
